87ba7dad33aeab22c951f6562ea74c837102fdff
GranLotoIroh <joseluiyanezmenendez@gmail.com>
2025-04-04 19:25:38 +0200

    04/04 Bordes shiny, fondo, boton generar de nuevo, colores

--- index.php
+++ index.php
@@ -2,19 +2,41 @@
 
 function getPokemonData($randID)
 {
-    $apicontentfromjson = file_get_contents("https://pokeapi.co/api/v2/pokemon/$randID");
-    $arrayapicontent = json_decode($apicontentfromjson, true);
-    $pokemonData = [
-        'name' => $arrayapicontent['name'],
-        'id' => $arrayapicontent['id'],
-        'image' => $arrayapicontent['sprites']['front_default'],
-        'types' => array_map(function ($type) {
-            return $type['type']['name'];
-        }, $arrayapicontent['types']),
-        'abilities' => array_map(function ($ability) {
-            return $ability['ability']['name'];
-        }, $arrayapicontent['abilities'])
-    ];
+    $randshiny=rand(1,20);
+    $randshiny2=rand(1,20);
+   /*  $randshiny=5;
+    $randshiny2=5; */
+    if($randshiny == $randshiny2){
+            $apicontentfromjson = file_get_contents("https://pokeapi.co/api/v2/pokemon/$randID");
+            $arrayapicontent = json_decode($apicontentfromjson, true);
+            $pokemonData = [
+            'name' => ucwords($arrayapicontent['name']),
+            'id' => $arrayapicontent['id'],
+            'image' => $arrayapicontent['sprites']['front_shiny'],
+            'types' => array_map(function ($type) {
+                return ucwords($type['type']['name']);
+            }, $arrayapicontent['types']),
+            'abilities' => array_map(function ($ability) {
+                return ucwords($ability['ability']['name']);
+            }, $arrayapicontent['abilities']),
+            'esshiny' => true
+        ];
+        }else{
+        $apicontentfromjson = file_get_contents("https://pokeapi.co/api/v2/pokemon/$randID");
+        $arrayapicontent = json_decode($apicontentfromjson, true);
+        $pokemonData = [
+            'name' => ucwords($arrayapicontent['name']),
+            'id' => $arrayapicontent['id'],
+            'image' => $arrayapicontent['sprites']['front_default'],
+            'types' => array_map(function ($type) {
+                return ucwords($type['type']['name']);
+            }, $arrayapicontent['types']),
+            'abilities' => array_map(function ($ability) {
+                return ucwords($ability['ability']['name']);
+            }, $arrayapicontent['abilities']),
+            'esshiny' => false  
+        ];
+    }
     return $pokemonData;
 }
 
@@ -23,16 +45,19 @@ function renderCards() {
     echo "<div class='caja'>";
     foreach ($randID as $id) {
         $pokeArray = getPokemonData($id);
-        echo "<div class='carta'>";
+        $bordershiny = $pokeArray["esshiny"] ? "esshiny" : "";
+        $types = $pokeArray['types'];
+        $typeClass = strtolower($types[0]);
+        echo "<div class='carta $bordershiny $typeClass' >";
         echo "<div class='img-container'>";
         echo "<img src='" . $pokeArray['image'] . "' alt='" . $pokeArray['name'] . "'>";
         echo "</div>";
         echo "<div class='datos'>";
         echo "<h3>" . $pokeArray['name'] . "</h3>";
-        echo "<h4>ID Pokemon " . $pokeArray['id'] . "</h4>";
+        echo "<h4>Pokedex Nº: " . $pokeArray['id'] . "</h4>";
         echo "<div class='tipos-pokemon'>";
         foreach ($pokeArray['types'] as $type) {
-            echo "<span>" . $type . "</span>";
+            echo "<span class='$type'>" . ucwords($type) . "</span>";
         }
         echo "</div>";
         echo "<ul class='habilidades'>";
@@ -40,6 +65,11 @@ function renderCards() {
             echo "<li>" . $ability . "</li>";
         }
         echo "</ul>";
+        if ($pokeArray["esshiny"]) {
+            echo "<div class='shinyimg'>";
+            echo "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Shiny_hex_logo.svg/512px-Shiny_hex_logo.svg.png' alt='Shiny'>";
+            echo "</div>";
+        }
         echo "</div>";
         echo "</div>";
     }
@@ -54,12 +84,16 @@ function renderCards() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PokeWeb</title>
+    <link href="https://fonts.googleapis.com/css2?family=Pokemon+Solid&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>PokeCartas</h1>
+    <h1>POKECARTAS</h1>
     <?php renderCards(); ?>
+    <div class="boton">
+    <a href="index.php" class="botongenerar">Generar nuevos pokemon</a>
+    </div>
 </body>
 
 </html>
